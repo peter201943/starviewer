025cfb3400461d747b53191ab493b328598f0195
Peter 201943 Mangelsdorf <pjm349@drexel.edu>
2022-04-17 19:51:16 -0400

    more notes and some fixes on web local script

--- starviewer/app/deployments/dev/local_server.py
+++ starviewer/app/deployments/dev/local_server.py
@@ -1,12 +1,14 @@
 # Simple script to make the contents of a local folder accessible via a browser
 import http.server
 import socketserver
-port = 8003
+from os import getcwd
+port = 8005
 handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", port), handler) as httpd:
-  print(f"serving this directory at https://localhost:{port}")
+  print(f"StarViewer: Serving \"{getcwd()}\" at \"localhost:{port}\"")
+  print("StarViewer: Kill with ctrl+c at any time")
   try:
     httpd.serve_forever()
   except KeyboardInterrupt:
-    print("shutting down")
+    print("StarViewer: Shutting Down")
     httpd.shutdown()
